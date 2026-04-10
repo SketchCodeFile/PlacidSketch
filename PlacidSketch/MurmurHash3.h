4035fb4891b4687666f2b9bf6d1437cae65c382d
DDD <18032287336@163.com>
2026-04-10 17:03:39 +0800

    Update MurmurHash3.h

--- PlacidSketch/MurmurHash3.h
+++ PlacidSketch/MurmurHash3.h
@@ -1,11 +1,3 @@
-/**
- * @file MurmurHash3.h
- * @brief MurmurHash3 哈希函数实现
- * 
- * MurmurHash3 was written by Austin Appleby, and is placed in the public
- * domain. The author hereby disclaims copyright to this source code.
- */
-
 #ifndef _MURMURHASH3_H_
 #define _MURMURHASH3_H_
 
